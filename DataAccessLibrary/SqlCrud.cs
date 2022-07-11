29b589eb5d841e7963630eb6d04b9ca90418b717
Sahil Baliyan <iamsahilbaliyan@gmail.com>
2022-07-11 22:26:33 +0530

    Add project files.

---
+++ DataAccessLibrary/SqlCrud.cs
@@ -0,0 +1,55 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SqlCrud
+    {
+        private readonly string connectionString;
+        private SqlDataAccess db = new SqlDataAccess(); 
+
+        public SqlCrud(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<BasicContactModel> GetAllContacts()
+        {
+            string sql = "select Id, FirstName, LastName from dbo.Contacts";
+
+            return db.LoadData<BasicContactModel,dynamic>(sql, new {},connectionString);
+        }
+
+        public FullContactModel GetFullContactById(int id)
+        {
+            string sql = "select Id, FirstName, LastName from dbo.Contacts where Id = @Id";
+            FullContactModel output = new FullContactModel();
+
+            output.BasicInfo = db.LoadData<BasicContactModel, dynamic>(sql, new { Id = id }, connectionString).FirstOrDefault();
+
+            if(output.BasicInfo == null)
+            {
+                return null;
+            }
+            sql = @"Select e.*
+                    from dbo.EmailAddresses e
+                    inner
+                    join dbo.ContactEmail ce on ce.EmailAddressId = e.Id
+                    where ce.ContactId = @Id";
+
+            output.EmailAddresses = db.LoadData<EmailAddressModel, dynamic >(sql, new { Id = id }, connectionString);
+
+            sql = @"Select p.*
+                    from dbo.PhoneNumbers p
+                    inner join dbo.ContactPhoneNumbers cp on cp.PhoneNumberId = p.Id
+                    where cp.ContactId = @Id";
+
+            output.PhoneNumbers = db.LoadData<PhoneNumberModel, dynamic>(sql, new { Id = id }, connectionString);
+            return output;
+        }
+    }
+}
---
+++ SQLServerUI/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using DataAccessLibrary;
+using DataAccessLibrary.Models;
+
+namespace SQLServerUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           SqlCrud sql = new SqlCrud(GetConnectionString());
+
+            //ReadAllContacts(sql);
+
+            ReadContacts(sql, 1);
+           Console.ReadLine();
+        }
+
+        private static void ReadAllContacts(SqlCrud sql)
+        {
+            var rows = sql.GetAllContacts();
+            foreach (var row in rows)
+            {
+                Console.WriteLine($"{row.Id}: {row.FirstName}{row.LastName}");
+            }
+        }
+
+        private static void ReadContacts(SqlCrud sql, int contactId)
+        {
+            var contact = sql.GetFullContactById(contactId);
+            Console.WriteLine($"{contact.BasicInfo.Id}: {contact.BasicInfo.FirstName}{contact.BasicInfo.LastName}");
+
+
+        }
+
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+
+            return output;
+        }
+    }
+}
